86dd71819d83913fa6adbd22bfad2fbb22e03609
Terry Brady <terrywbrady@gmail.com>
2019-02-05 08:58:34 -0800

    dspaceconfig

--- src/main/java/my/test/Test.java
+++ src/main/java/my/test/Test.java
@@ -1,13 +1,25 @@
 package my.test;
 
+import java.io.File;
+import java.util.Iterator;
+
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.EnvironmentConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.DefaultConfigurationBuilder;
 
 public class Test {
 
     public static void main(String[] args) {
-        Configuration config = new EnvironmentConfiguration();
-        System.out.println(config.getString("dspace.foo"));
+        //builder.setFile(new File("config.xml"));
+        try {
+            DefaultConfigurationBuilder builder = new DefaultConfigurationBuilder("config-definition.xml");
+            Configuration config = builder.getConfiguration(true);
+            System.out.println(config.getString("dspace.foo"));
+            System.out.println(config.getString("dspace.name"));
+        } catch (ConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
