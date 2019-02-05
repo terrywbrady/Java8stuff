bc71a51e9e0bfb36c424a80e52c3f3782018e654
Terry Brady <terrywbrady@gmail.com>
2019-02-04 19:15:32 -0800

    v1 config test

--- src/main/java/my/test/Test.java
+++ src/main/java/my/test/Test.java
@@ -1,10 +1,13 @@
 package my.test;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.EnvironmentConfiguration;
+
 public class Test {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Configuration config = new EnvironmentConfiguration();
+        System.out.println(config.getString("dspace.foo"));
     }
 
 }
