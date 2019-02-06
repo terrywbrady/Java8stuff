90be7335acb969e31f52bed2b04960a1bb55fd80
Terry Brady <terrywbrady@gmail.com>
2019-02-05 16:15:32 -0800

    improve testing

--- src/main/java/my/test/Test.java
+++ src/main/java/my/test/Test.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.DefaultConfigurationBuilder;
+import org.apache.commons.configuration.EnvironmentConfiguration;
 
 public class Test {
 
@@ -14,6 +15,22 @@ public class Test {
         try {
             DefaultConfigurationBuilder builder = new DefaultConfigurationBuilder("config-definition.xml");
             Configuration config = builder.getConfiguration(true);
+            for(Iterator<String> keys = config.getKeys(); keys.hasNext(); ) {
+                System.out.println("TBTB1 "+keys.next());
+            }
+            
+            System.out.println("");
+            
+            for(Iterator<String> keys=new EnvironmentConfiguration().getKeys(); keys.hasNext();) {
+                System.out.println("TBTB2 "+keys.next());
+            }
+            
+            System.out.println("");
+            
+            for(String s: System.getenv().keySet()) {
+                System.out.println("TBTB3"+s);
+            }
+            
             System.out.println(config.getString("dspace.foo"));
             System.out.println(config.getString("dspace.name"));
         } catch (ConfigurationException e) {
